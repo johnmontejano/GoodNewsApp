06829eed1d95aa9a42405fc7a8f110c71782ee12
johnmontewhat <us4john@gmail.com>
2016-07-15 14:19:24 -0700

    good news commit

---
+++ goodnews/WebViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WebViewController.swift
+//  goodnews
+//
+//  Created by John Montejano on 7/13/16.
+//  Copyright © 2016 John Montejano. All rights reserved.
+//
+
+import Cocoa
+
+class WebViewController: UIViewController {
+
+}
